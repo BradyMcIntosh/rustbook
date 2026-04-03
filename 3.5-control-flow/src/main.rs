05e99e2041685218205381b510687a6393ff17db
BradyMcIntosh <bradymcintosh@noreply.codeberg.org>
2026-04-03 12:56:55 -0400

    Add loop work

--- 3.5-control-flow/src/main.rs
+++ 3.5-control-flow/src/main.rs
@@ -41,4 +41,79 @@ fn main() {
     let number4 = if condition { 8 } else { 9 };
 
     println!("Since condition is {condition}, the next number is {number4}");
+
+
+    println!("Here's a value that starts at 0, increments 10 times, then doubles...");
+
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+
+    println!("Now let's do some counting...");
+
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    println!("Ready to launch?");
+
+    let mut number = 3;
+
+    while number != 0 {
+        println!("{number}!");
+
+        number -= 1;
+    }
+
+    println!("LIFTOFF!!!");
+
+    println!("Here's my collection");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("the value of {0} is: {1}", index, a[index]);
+
+        index += 1;
+    }
+
+    println!("Here's my collection, but in a for loop :)");
+
+    for element in a {
+        println!("the value is: {element}");
+    }
+
+    println!("And here's another launch.");
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+    println!("LIFTOFF!!!");
+
+    let b = [1;2];
 }
