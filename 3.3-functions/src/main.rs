ff948407814430d74f359b6f082e21e27a120ca6
BradyMcIntosh <bradymcintosh@noreply.codeberg.org>
2026-04-02 23:10:52 -0400

    Add function work

--- 3.3-functions/src/main.rs
+++ 3.3-functions/src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     another_function();
     number_function(5);
     print_labeled_measurement(5, 'h');
+
+    let x = five();
+
+    println!("The return value of x is: {x}");
+
+    let y = plus_one(5);
+    println!("The value of 5 + 1 is: {y}");
 }
 
 fn another_function() {
@@ -16,4 +23,12 @@ fn number_function(x: i32) {
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
     println!("The measurement is: {value}{unit_label}");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
