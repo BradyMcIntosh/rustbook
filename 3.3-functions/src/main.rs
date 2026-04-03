62c3d91af3a63d4f59b9f18f614a8dd8b37b655b
BradyMcIntosh <bradymcintosh@noreply.codeberg.org>
2026-04-02 21:03:32 -0400

    Add function work

--- 3.3-functions/src/main.rs
+++ 3.3-functions/src/main.rs
@@ -2,8 +2,18 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    number_function(5);
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function() {
     println!("Another function.");
+}
+
+fn number_function(x: i32) {
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
