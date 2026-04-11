bf77157461ffe0afc71c8c0df1da2ccfbed9d4f1
BradyMcIntosh <bradym.git@gmail.com>
2026-04-10 20:11:43 -0400

    Add slice work

--- 4.4-slice-type/src/main.rs
+++ 4.4-slice-type/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+
+    let hello: &str = &s[0..5];
+    let world: &str = &s[6..11];
+
+    println!("slices: '{hello}' & '{world}'");
+
+    let slice1: &str = &s[..5];
+    let slice2: &str = &s[6..];
+    let slice3: &str = &s[..];
+    let slice4: &str = &s[..2];
 }
