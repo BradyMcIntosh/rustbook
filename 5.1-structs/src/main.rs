b48e4e2bc5216865b2f56b372ee57270064e044c
BradyMcIntosh <bradym.git@gmail.com>
2026-04-12 13:26:08 -0400

    Add struct work

--- 5.1-structs/src/main.rs
+++ 5.1-structs/src/main.rs
@@ -21,6 +21,10 @@ fn main() {
     user2.sign_in_count = 3;
 
     println!("User2: '{0}' at '{1}'; active: {2}; sign-in count: {3}", user2.username, user2.email, user2.active, user2.sign_in_count);
+
+    let user3 = build_user(String::from("thirdguy@example.com"), String::from("randomname789"));
+
+    println!("User3: '{0}' at '{1}'; active: {2}; sign-in count: {3}", user3.username, user3.email, user3.active, user3.sign_in_count);
 }
 
 struct User {
@@ -28,4 +32,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
