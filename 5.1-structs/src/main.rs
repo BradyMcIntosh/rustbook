6e078bd028d490a1a0db82664c493ca7be22104a
BradyMcIntosh <bradym.git@gmail.com>
2026-04-12 13:16:24 -0400

    Add structs work

--- 5.1-structs/src/main.rs
+++ 5.1-structs/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    println!("User1: '{0}' at '{1}'; active: {2}; sign-in count: {3}", user1.username, user1.email, user1.active, user1.sign_in_count);
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
