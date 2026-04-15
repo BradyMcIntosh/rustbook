251da912861f6da440423de722f70dde185beeb0
BradyMcIntosh <bradym.git@gmail.com>
2026-04-14 22:11:42 -0400

    Add enums work

--- 6.1-enums/src/main.rs
+++ 6.1-enums/src/main.rs
@@ -1,24 +1,11 @@
 enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
-    let loopback = IpAddr {
-        kind: IpAddrKind::V6,
-        address: String::from("::1"),
-    };
-
-    println!("home address: '{}'", &home.address);
-    println!("loopback address: '{}'", &loopback.address);
+    // can't get the inner values yet :)
 }
