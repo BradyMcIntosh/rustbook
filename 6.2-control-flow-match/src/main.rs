95ca365dce906f1ed89a82626428d6f233802341
BradyMcIntosh <bradym.git@gmail.com>
2026-04-16 17:34:08 -0400

    Add match work

--- 6.2-control-flow-match/src/main.rs
+++ 6.2-control-flow-match/src/main.rs
@@ -33,7 +33,7 @@ fn plus_one(x: Option<i32>) -> Option<i32> {
 
 fn add_fancy_hat() {}
 fn remove_fancy_hat() {}
-fn move_player(num_spaces: u8) {}
+fn reroll() {}
 
 fn main() {
     let penny = Coin::Penny;
@@ -61,6 +61,6 @@ fn main() {
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        other => move_player(other),
+        _ => reroll(),
     }
 }
