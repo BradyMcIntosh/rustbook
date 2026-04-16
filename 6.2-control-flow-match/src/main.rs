c824ea885c9b3eb76482e7be227c2627586dc057
BradyMcIntosh <bradym.git@gmail.com>
2026-04-16 17:22:05 -0400

    Add match work

--- 6.2-control-flow-match/src/main.rs
+++ 6.2-control-flow-match/src/main.rs
@@ -1,3 +1,27 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let penny = Coin::Penny;
+    let nickel = Coin::Nickel;
+    let dime = Coin::Dime;
+    let quarter = Coin::Quarter;
+
+    println!("Penny   = {}c", value_in_cents(penny));
+    println!("Nickel  = {}c", value_in_cents(nickel));
+    println!("Dime    = {}c", value_in_cents(dime));
+    println!("Quarter = {}c", value_in_cents(quarter));
 }
