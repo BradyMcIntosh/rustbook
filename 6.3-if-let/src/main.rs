fc1cb57f8d33baad7936895c2a28ef4061e55b73
BradyMcIntosh <bradym.git@gmail.com>
2026-04-16 21:13:35 -0400

    Add if let work

--- 6.3-if-let/src/main.rs
+++ 6.3-if-let/src/main.rs
@@ -2,5 +2,7 @@ fn main() {
     let config_max = Some(3u8);
     if let Some(max) = config_max {
         println!("The maximum is configured to be {max}");
+    } else {
+        println!("The maximum is nothing.");
     }
 }
