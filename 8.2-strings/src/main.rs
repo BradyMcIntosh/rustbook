16b502e53bb7a30f268e1b84d393d7e948eb057f
BradyMcIntosh <bradym.git@gmail.com>
2026-04-18 16:51:10 -0400

    Add strings work

--- 8.2-strings/src/main.rs
+++ 8.2-strings/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut hw = String::new();
+    hw.push_str("Hello");
+    hw.push_str(" ");
+    hw.push_str("world");
+    hw.push_str("!");
+
+    println!("{hw}");
+
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2);
+    println!("s1 is {s1}");
 }
