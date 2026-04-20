adab5cf1569149eb6d622b2c389cfbce376bd42d
BradyMcIntosh <bradym.git@gmail.com>
2026-04-19 22:51:16 -0400

    Add results work

--- 9.2-results/src/main.rs
+++ 9.2-results/src/main.rs
@@ -1,5 +1,21 @@
 use std::fs::File;
-use std::io::ErrorKind;
+use std::io::{self, ErrorKind, Read};
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let username_file_result = File::open("username.txt");
+
+    let mut username_file = match username_file_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut username = String::new();
+
+    match username_file.read_to_string(&mut username) {
+        Ok(_) => Ok(username),
+        Err(e) => Err(e),
+    }
+}
 
 fn main() {
     let greeting_file_result = File::open("hello.txt");
@@ -22,6 +38,9 @@ fn main() {
 
     println!("File has been opened: hello.txt");
 
-    let _greeting_file_2 = File::open("world.txt")
-        .expect("world.txt could not be opened");
+    // vvvv this code will panic
+    // let _greeting_file_2 = File::open("world.txt")
+    //     .expect("world.txt could not be opened");
+
+    read_username_from_file().expect("could not read username from file...");
 }
