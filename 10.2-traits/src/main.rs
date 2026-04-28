ca297e227f23cb16c112b884bc1b436404241dfa
BradyMcIntosh <bradym.git@gmail.com>
2026-04-28 17:00:03 -0400

    Add traits work

--- 10.2-traits/src/main.rs
+++ 10.2-traits/src/main.rs
@@ -1,3 +1,14 @@
+use traits::{SocialPost, Summary};
+
 fn main() {
-    println!("Hello, world!");
+    let post = SocialPost {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people"
+        ),
+        reply: false,
+        repost: false,
+    };
+
+    println!("1 new post: {}", post.summarize());
 }
