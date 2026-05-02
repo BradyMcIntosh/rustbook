fd471d6268e5b708aa1326b62d8510a213ea2901
BradyMcIntosh <bradym.git@gmail.com>
2026-05-02 14:46:37 -0400

    Add tests work

--- 11.1-tests-1/src/lib.rs
+++ 11.1-tests-1/src/lib.rs
@@ -2,6 +2,18 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -16,4 +28,18 @@ mod tests {
     // fn another() {
     //     panic!("This test explodes!")
     // }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
 }
