8d3df534b657931e69c04474c6d207594670ca9a
BradyMcIntosh <bradym.git@gmail.com>
2026-05-02 11:31:13 -0400

    Add lifetimes work

--- 10.3-lifetimes/src/main.rs
+++ 10.3-lifetimes/src/main.rs
@@ -2,10 +2,22 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() { x } else { y }
 }
 
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
 fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {result}!");
+
+    let novel = String::from("It was the best of times, it was the worst of times. The end.");
+    let first_sentence = novel.split(".").next().unwrap();
+    let i = ImportantExcerpt {
+        part: first_sentence
+    };
+
+    println!("First sentence of novel: '{0}'", i.part);
 }
