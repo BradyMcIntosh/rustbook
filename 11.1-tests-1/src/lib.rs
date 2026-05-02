a3ee973e604d36bac2ed92624328cc77f810e48c
BradyMcIntosh <bradym.git@gmail.com>
2026-05-02 14:52:36 -0400

    Add tests work

--- 11.1-tests-1/src/lib.rs
+++ 11.1-tests-1/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
+pub fn add_two(a: usize) -> usize {
+    a + 2
+}
+
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
@@ -21,6 +25,12 @@ mod tests {
     #[test]
     fn exploration() {
         let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
         assert_eq!(result, 4);
     }
 
