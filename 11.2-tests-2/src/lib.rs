bc722732892ceaee93652e98fb52989e95b413f8
BradyMcIntosh <bradym.git@gmail.com>
2026-05-06 21:55:28 -0400

    Add tests-2 work

--- 11.2-tests-2/src/lib.rs
+++ 11.2-tests-2/src/lib.rs
@@ -3,6 +3,10 @@ fn prints_and_returns_10(a: i32) -> i32 {
     10
 }
 
+pub fn add_two(a: u64) -> u64 {
+    a + 2
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -17,5 +21,23 @@ mod tests {
     fn this_test_will_fail() {
         let value = prints_and_returns_10(8);
         assert_eq!(value, 5);
+    }
+
+    #[test]
+    fn add_two_and_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        let result = add_two(3);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn one_hundred() {
+        let result = add_two(100);
+        assert_eq!(result, 102);
     }
 }
