c5003b59f1402355d22ab0bb05ea548514e98c97
BradyMcIntosh <bradym.git@gmail.com>
2026-05-06 22:15:29 -0400

    Add tests-3 work

--- 11.3-tests-3/src/lib.rs
+++ 11.3-tests-3/src/lib.rs
@@ -1,4 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub fn add_two(x: u64) -> u64 {
+    internal_adder(x, 2)
+}
+
+fn internal_adder(left: u64, right: u64) -> u64 {
     left + right
 }
 
@@ -8,7 +12,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = add_two(2);
         assert_eq!(result, 4);
     }
 }
