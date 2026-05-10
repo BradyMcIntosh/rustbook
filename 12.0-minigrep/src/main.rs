7bda7bff0a6c773e77f31c6e470cee8da2044d4f
BradyMcIntosh <bradym.git@gmail.com>
2026-05-10 13:09:19 -0400

    Format

--- 12.0-minigrep/src/main.rs
+++ 12.0-minigrep/src/main.rs
@@ -9,7 +9,8 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
 }
@@ -19,9 +20,9 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config( args: &[String] ) -> Config {
+fn parse_config(args: &[String]) -> Config {
     let query = args[1].clone();
     let file_path = args[2].clone();
 
-    Config {query, file_path}
+    Config { query, file_path }
 }
