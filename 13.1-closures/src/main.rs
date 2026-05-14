34c6e2078401fa28700bd3f2d87b1a6f0fdc39ad
BradyMcIntosh <bradym.git@gmail.com>
2026-05-13 21:56:22 -0400

    Add closures work

--- 13.1-closures/src/main.rs
+++ 13.1-closures/src/main.rs
@@ -59,4 +59,13 @@ fn main() {
     };
 
     println!("{} has been calculated.", expensive_closure(2));
+
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {list:?}");
+
+    let only_borrows = || println!("From closure: {list:?}");
+
+    println!("Before calling closure: {list:?}");
+    only_borrows();
+    println!("After calling closure: {list:?}");
 }
