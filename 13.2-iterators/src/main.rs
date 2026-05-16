8e873597bf264cd0cff39a043d9c735952bfa633
BradyMcIntosh <bradym.git@gmail.com>
2026-05-15 21:47:41 -0400

    Add iterators work

--- 13.2-iterators/src/main.rs
+++ 13.2-iterators/src/main.rs
@@ -6,6 +6,13 @@ fn main() {
     for val in v1_iter {
         println!("Got: {val}");
     }
+
+    let v2: Vec<i32> = vec![1, 2, 3];
+
+    let v3: Vec<_> = v2.iter().map(|x| x + 1).collect();
+
+    assert_eq!(v3, vec![2, 3, 4]);
+    println!("Vector 3 = {v3:?}");
 }
 
 #[cfg(test)]
