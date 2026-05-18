334d2c02cabf74844f6b9daa35d3c1afb69d8185
BradyMcIntosh <bradym.git@gmail.com>
2026-05-18 16:01:38 -0400

    Add learn-cargo work

--- 14.0-learn-cargo/src/lib.rs
+++ 14.0-learn-cargo/src/lib.rs
@@ -16,3 +16,34 @@
 pub fn sum(x: i32, y: i32) -> i32 {
     x + y
 }
+
+pub mod kinds {
+    /// The classic numbers everyone should know.
+    pub enum EasyNumber {
+        One,
+        Two,
+        Three,
+    }
+
+    /// Rare numbers known only to the erudite or insane.
+    pub enum HardNumber {
+        Nine,
+        Ten,
+        Eleven,
+    }
+}
+
+pub mod utils {
+    use std::fmt::Error;
+
+    use crate::kinds::*;
+
+    /// Attempts to add a hard number to an easy number. Dangerous business.
+    ///
+    /// ### Panics
+    ///
+    /// This function will panic if it can't count high enough.
+    pub fn add(num1: EasyNumber, num2: HardNumber) -> Result<Error, HardNumber> {
+        unimplemented!();
+    }
+}
