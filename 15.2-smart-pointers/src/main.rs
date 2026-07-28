5def013a008d9962241bfdc7508f513ed2118b27
BradyMcIntosh <bradym.git@gmail.com>
2026-07-28 15:01:52 -0400

    Add smart pointers work

--- 15.2-smart-pointers/src/main.rs
+++ 15.2-smart-pointers/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    let y1 = &x;
+
+    assert_eq!(5, x);
+    assert_eq!(x, *y1);
+
+    let y2 = Box::new(x);
+    assert_eq!(x, *y2);
 }
