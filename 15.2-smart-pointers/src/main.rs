8fba0fe7346366795b82362095c99870968dfce4
BradyMcIntosh <bradym.git@gmail.com>
2026-08-09 16:19:40 -0400

    Add smart pointers work

--- 15.2-smart-pointers/src/main.rs
+++ 15.2-smart-pointers/src/main.rs
@@ -1,3 +1,21 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
     let x = 5;
     let y1 = &x;
@@ -7,4 +25,10 @@ fn main() {
 
     let y2 = Box::new(x);
     assert_eq!(x, *y2);
+
+    let my_x = 5;
+    let my_y = MyBox::new(x);
+
+    assert_eq!(5, my_x);
+    assert_eq!(my_x, *my_y);
 }
