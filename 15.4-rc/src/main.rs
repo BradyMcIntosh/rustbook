3ea5c4d8e0e4eb73e20bb1540e27f968971d1878
BradyMcIntosh <bradym.git@gmail.com>
2026-08-10 16:12:03 -0400

    Add rc work

--- 15.4-rc/src/main.rs
+++ 15.4-rc/src/main.rs
@@ -12,6 +12,9 @@ fn main() {
     println!("count after creating a = {}", Rc::strong_count(&a));
     let b = Cons(3, Rc::clone(&a));
     println!("count after creating b = {}", Rc::strong_count(&a));
-    let c = Cons(4, Rc::clone(&a));
-    println!("count after creating c = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
