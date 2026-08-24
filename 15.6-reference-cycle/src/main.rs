6ea71626aec10ccdbab0b774d1725737da27a3ce
BradyMcIntosh <bradym.git@gmail.com>
2026-08-24 12:26:45 -0400

    Add reference cycle work

--- 15.6-reference-cycle/src/main.rs
+++ 15.6-reference-cycle/src/main.rs
@@ -1,5 +1,5 @@
 use crate::List::{Cons, Nil};
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, rc::{Rc, Weak}};
 
 #[derive(Debug)]
 enum List {
@@ -19,6 +19,7 @@ impl List {
 #[derive(Debug)]
 struct Node {
     value: i32,
+    parent: RefCell<Weak<Node>>,
     children: RefCell<Vec<Rc<Node>>>,
 }
 
@@ -48,11 +49,19 @@ fn main() {
     // Nodes
     let leaf = Rc::new(Node {
         value: 3,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![]),
     });
 
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
     let branch = Rc::new(Node {
         value: 5,
+        parent: RefCell::new(Weak::new()),
         children: RefCell::new(vec![Rc::clone(&leaf)]),
     });
+
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 }
